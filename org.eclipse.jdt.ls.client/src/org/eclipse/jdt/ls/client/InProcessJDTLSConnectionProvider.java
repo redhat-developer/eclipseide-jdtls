dcfecfb641ff3ba7ce86de5445095fd944831ef9
Mickael Istria <mistria@redhat.com>
2022-11-08 17:23:10 +0100

    Do not use not (yet) existing symbol for JDT-LS

--- org.eclipse.jdt.ls.client/src/org/eclipse/jdt/ls/client/InProcessJDTLSConnectionProvider.java
+++ org.eclipse.jdt.ls.client/src/org/eclipse/jdt/ls/client/InProcessJDTLSConnectionProvider.java
@@ -5,6 +5,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.nio.channels.Channels;
 import java.nio.channels.Pipe;
 import java.nio.charset.StandardCharsets;
@@ -14,11 +15,10 @@ import java.util.concurrent.Future;
 
 import org.eclipse.jdt.ls.core.internal.JavaClientConnection.JavaLanguageClient;
 import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
+import org.eclipse.jdt.ls.core.internal.handlers.BaseDocumentLifeCycleHandler;
 import org.eclipse.jdt.ls.core.internal.handlers.JDTLanguageServer;
 import org.eclipse.lsp4e.server.StreamConnectionProvider;
 import org.eclipse.lsp4j.jsonrpc.Launcher;
-import org.eclipse.lsp4j.launch.LSPLauncher;
-import org.eclipse.lsp4j.services.LanguageClient;
 
 public class InProcessJDTLSConnectionProvider implements StreamConnectionProvider {
 
@@ -30,6 +30,19 @@ public class InProcessJDTLSConnectionProvider implements StreamConnectionProvide
 
 	@Override
 	public void start() throws IOException {
+		// Workaround: too much log; JDT-LS forces streams (part 1)
+		InputStream in = System.in;
+		PrintStream out = System.out;
+		PrintStream err = System.err;
+		// end workaround
+		// Blocker: https://github.com/eclipse/eclipse.jdt.ls/issues/2312
+		// Blocker: https://github.com/eclipse/eclipse.jdt.ls/issues/2311
+		// Blocker: https://github.com/eclipse/eclipse.jdt.ls/issues/2309
+		// Major: https://github.com/eclipse/eclipse.jdt.ls/issues/2310
+		// see https://github.com/eclipse/eclipse.jdt.ls/issues/2309
+		System.setProperty("GENERATES_METADATA_FILES_AT_PROJECT_ROOT", Boolean.TRUE.toString());
+		// see https://github.com/eclipse/eclipse.jdt.ls/issues/2312
+		System.setProperty(/*BaseDocumentLifeCycleHandler.SKIP_CHANGE_UPDATE*/BaseDocumentLifeCycleHandler.class.getName() + ".SKIP_CHANGE_UPDATE", Boolean.TRUE.toString());
 		Pipe serverOutputToClientInput = Pipe.open();
 		Pipe clientOutputToServerInput = Pipe.open();
 		errorStream = new ByteArrayInputStream("Error output on console".getBytes(StandardCharsets.UTF_8));
@@ -37,17 +50,23 @@ public class InProcessJDTLSConnectionProvider implements StreamConnectionProvide
 		InputStream serverInputStream = Channels.newInputStream(clientOutputToServerInput.source());
 		OutputStream serverOutputStream = Channels.newOutputStream(serverOutputToClientInput.sink());
 		JDTLanguageServer server = new org.eclipse.jdt.ls.core.internal.handlers.JDTLanguageServer(JavaLanguageServerPlugin.getProjectsManager(), JavaLanguageServerPlugin.getPreferencesManager());
+		// Workaround: too much log; JDT-LS forces streams (part 2)
+		// restore initial streams
+		System.setIn(in);
+		System.setOut(out);
+		System.setErr(err);
+		// end workaround
 		JavaLanguageServerPlugin.getInstance().setProtocol(server);
-		Launcher<LanguageClient> launcher = null;
+		Launcher<JavaLanguageClient> launcher = null;
 		try {
-			launcher = LSPLauncher.createServerLauncher(server, serverInputStream,
-				serverOutputStream);
+			launcher = Launcher.createLauncher(server, JavaLanguageClient.class, serverInputStream, serverOutputStream);
 		} catch (Throwable t) {
 			throw new IOException(t);
 		}
 		clientInputStream = Channels.newInputStream(serverOutputToClientInput.source());
 		clientOutputStream = Channels.newOutputStream(clientOutputToServerInput.sink());
 		listener = launcher.startListening();
+		server.connectClient((JavaLanguageClient)launcher.getRemoteProxy());
 		
 		// Store the output streams so we can close them to clean up. The corresponding input
 		// streams should automatically receive an EOF and close.
