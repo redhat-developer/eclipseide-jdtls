a969cc4d34ede0db174a332ebe529948c1dbba30
Mickael Istria <mistria@redhat.com>
2023-02-02 21:14:42 +0100

    Hide jdl.ls-java-project by default

---
+++ com.redhat.eclipseide.jdtlsclient/src/com/redhat/eclipseide/jdtlsclient/FilterJDTLSProjectInNavigator.java
@@ -0,0 +1,20 @@
+package com.redhat.eclipseide.jdtlsclient;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.Adapters;
+import org.eclipse.jdt.ls.core.internal.managers.ProjectsManager;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+public class FilterJDTLSProjectInNavigator extends ViewerFilter {
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		IProject project = Adapters.adapt(element, IProject.class);
+		if (project == null) {
+			return true;
+		}
+		return !project.getName().equals(ProjectsManager.DEFAULT_PROJECT_NAME);
+	}
+
+}
