cdf4f8228b9fbfa112cd77e1e0d3d74d17e831ce
Mickael Istria <mistria@redhat.com>
2023-04-27 21:43:09 +0200

    Add test for syserr completion

---
+++ com.redhat.eclipseide.jdtlsclient.test/src/com/redhat/eclipseide/jdtlsclient/test/TestCompletion.java
@@ -0,0 +1,111 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc.. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+package com.redhat.eclipseide.jdtlsclient.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.lsp4e.operations.completion.LSCompletionProposal;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Widget;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.tests.harness.util.DisplayHelper;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.ui.texteditor.ContentAssistAction;
+import org.eclipse.ui.texteditor.ITextEditorActionConstants;
+import org.junit.jupiter.api.Test;
+
+class TestCompletion {
+
+	@Test
+	public void testSyserr() throws Exception {
+		IProject project = (IProject) ResourcesPlugin.getWorkspace().getRoot().getProject(Long.toString(System.nanoTime()));
+		
+		project.create(TestLSPIntegration.projectDescWithJavaNature(project), null);
+		project.open(null);
+		IFolder srcFolder = project.getFolder("src");
+		srcFolder.create(true, true, null);
+		IJavaProject javaProject = JavaCore.create(project);
+		javaProject.setRawClasspath(new IClasspathEntry[] {
+			JavaCore.newSourceEntry(srcFolder.getFullPath()),
+			JavaRuntime.getDefaultJREContainerEntry()
+		}, null);
+		IFile javaFile = srcFolder.getFile("a.java");
+		javaFile.create(new ByteArrayInputStream("""
+			public class a {
+				void hello() {
+					syser
+				}
+			}
+			""".getBytes()), false, null);
+		IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		AbstractTextEditor editor = (AbstractTextEditor)IDE.openEditor(activePage, javaFile, "org.eclipse.ui.genericeditor.GenericEditor");
+		Display display = activePage.getWorkbenchWindow().getShell().getDisplay();
+		assertTrue(DisplayHelper.waitForCondition(display, 30000L, () -> {
+				try {
+					return javaFile.findMarkers("org.eclipse.lsp4e.diagnostic", true, IResource.DEPTH_ZERO).length > 0;
+				} catch (CoreException e) {	}
+				return false;
+			}), "Diagnostics not received from JDT-LS");
+		IDocument document = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+		int afterSyser = document.get().indexOf("syser") + "syser".length();
+		editor.getSelectionProvider().setSelection(new TextSelection(afterSyser, 0));
+		ContentAssistAction completionAction = (ContentAssistAction)editor.getAction(ITextEditorActionConstants.CONTENT_ASSIST);
+		completionAction.update();
+		Set<Shell> beforeShells = Set.of(display.getShells());
+		completionAction.run();
+		AtomicReference<LSCompletionProposal> syserrItem = new AtomicReference<>();
+		DisplayHelper.waitForCondition(display, 1500000, () -> {
+			Set<Shell> afterShell = new HashSet<>(Set.of(display.getShells()));
+			afterShell.removeAll(beforeShells);
+			Optional<LSCompletionProposal> maybe = afterShell.stream().map(TestLSPIntegration::findCompletionSelectionControl)
+				.filter(Objects::nonNull)
+				.flatMap(completionTable -> Stream.of(completionTable.getItems()).filter(item -> item.getText().contains("syserr")))
+				.map(Widget::getData)
+				.filter(LSCompletionProposal.class::isInstance)
+				.map(LSCompletionProposal.class::cast)
+				.findAny();
+			maybe.ifPresent(syserrItem::set);
+			return maybe.isPresent();
+		});
+		syserrItem.get().apply(editor.getAdapter(ITextViewer.class), '\n', 0, afterSyser);
+		assertEquals("""
+			public class a {
+				void hello() {
+					System.err.println();
+				}
+			}
+			""", document.get());
+	};
+
+}
